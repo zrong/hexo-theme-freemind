ce21fb26952c3c7c36e48c8a7ed25ef04d84e41f
zrong <zrongzrong@gmail.com>
2018-01-28 12:57:10 +0800

    调整下载功能

--- scripts/embed.js
+++ scripts/embed.js
@@ -6,7 +6,7 @@ const util = require('util')
 const dirname = pathFn.dirname(__filename)
 
 function getRid (pre='rid-') {
-  return pre + parseInt(Math.random()*99999)
+  return pre + parseInt(Math.random() * 99999)
 }
 
 function flash (args, content) {
@@ -28,23 +28,21 @@ function flash (args, content) {
     obj.fversion = 100
   }
 
-  //console.log('yaml: %j', obj)
+  // console.log('yaml: %j', obj)
   var templateFile = pathFn.join(dirname, '../layout/_embed/flash.ejs')
   var text = hexo.render.renderSync({path: templateFile}, obj)
 	return text
 }
 
-function download (args, content){
+function download (args, content) {
   var obj = yaml.safeLoad(content)
   var dlConfig = hexo.theme.config.embed ? hexo.theme.config.embed.download : null
   if (!dlConfig) {
     var msg = util.format('找不到 download 的配置信息:%s, args:%j', content, args)
     throw Error(msg)
   }
-  obj.infoUrl = util.format(dlConfig.info, obj.id)
-  obj.goUrl = util.format(dlConfig.go, obj.id)
-  if (!obj.rid) {
-    obj.rid = 'dl-'+obj.id
+  if (obj.id === undefined) {
+    return ''
   }
   if (!obj.title) {
     obj.title = '下载'
